d197421b74f0026b50a37ba478d2e16799a9722c
Jaemin Han <j4emin.han@gmail.com>
2021-07-04 03:46:42 -0400

    finish initial navbar styling

--- hobbynet/src/components/Navbar.js
+++ hobbynet/src/components/Navbar.js
@@ -11,9 +11,11 @@ import {
   MenuList, 
   MenuButton,
   MenuItem,
-  MenuDivider
+  MenuDivider,
+  Image,
+  Center
 } from "@chakra-ui/react";
-import { HamburgerIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, ChevronDownIcon, ChatIcon, BellIcon, ViewIcon } from "@chakra-ui/icons";
 
 const Navbar = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -32,48 +34,65 @@ const Navbar = (props) => {
     >
       <Flex align="center" mr={5}>
         <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-          Chakra UI
+          LOREM IPSUM
         </Heading>
       </Flex>
 
       <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
         <HamburgerIcon />
       </Box>
-
       <Stack
-        direction={{ base: "column", md: "row" }}
-        display={{ base: isOpen ? "block" : "none", md: "flex" }}
-        width={{ base: "full", md: "auto" }}
-        alignItems="center"
-        flexGrow={1}
-        mt={{ base: 4, md: 0 }}
+        direction="row"
+        align="center"
+        justify="space-between"
+        spacing="50px"
       >
-        <Text>Docs</Text>
-        <Text>Examples</Text>
-        <Text>Blog</Text>
-      </Stack>
+             <Stack
+                direction={{ base: "column", md: "row" }}
+                display={{ base: isOpen ? "block" : "none", md: "flex" }}
+                width={{ base: "full", md: "auto" }}
+                alignItems="center"
+                mt={{ base: 4, md: 0 }}
+                spacing="50px"
+            >   
+                <Stack display="flex" alignItems="center" spacing="0px" _hover={{opacity: 0.85}}>
+                    <ViewIcon align="center"></ViewIcon>
+                    <Text>Browse</Text>
+                </Stack>
+                <Stack display="flex" alignItems="center" spacing="0px" _hover={{opacity: 0.85}}>
+                    <BellIcon align="center"></BellIcon>
+                    <Text>Notifications</Text>
+                </Stack>
+                <Stack display="flex" alignItems="center" spacing="0px" _hover={{opacity: 0.85}}>
+                    <ChatIcon align="center"></ChatIcon>
+                    <Text>Messages</Text>
+                </Stack>
 
-      <Box
-        display={{ base: isOpen ? "block" : "none", md: "block" }}
-        mt={{ base: 4, md: 0 }}
-      >
-      </Box>
-      <Menu>
-        {({ isOpen }) => (
-            <>
-            <MenuButton color="black" isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />}>
-                Moses Kim
-            </MenuButton>
-            <MenuList color="black">
-                <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Profile</MenuItem>
-                <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Calender</MenuItem>
-                <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Settings</MenuItem>
-                <MenuDivider/>
-                <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Logout</MenuItem>
-            </MenuList>
-            </>
-        )}
-    </Menu>
+            </Stack>
+            <Stack _hover={{opacity: 0.85}}>
+                <Menu>
+                    <MenuButton>
+                        <Stack direction="row">
+                            <Image
+                                boxSize="2rem"
+                                borderRadius="full"
+                                src="https://placekitten.com/100/100"
+                                alt="Fluffybuns the destroyer"
+                                mr="5px"
+                            />
+                            <Center>Moses Kim<ChevronDownIcon ml="5px"></ChevronDownIcon></Center>
+                        </Stack>
+                    </MenuButton>
+                    <MenuList color="black">
+                        <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Profile</MenuItem>
+                        <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Calender</MenuItem>
+                        <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Settings</MenuItem>
+                        <MenuDivider/>
+                        <MenuItem onClick={() => window.location.replace("http://www.w3schools.com")}>Logout</MenuItem>
+                    </MenuList>           
+                </Menu>
+            </Stack>
+      </Stack>
     </Flex>
   );
 };
