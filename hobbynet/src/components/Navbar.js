b836fda773760a8e38e22848d797efc979d7d2f1
Jaemin Han <j4emin.han@gmail.com>
2021-07-03 22:04:41 -0400

    initial commit (deleted cra boilerplate)

---
+++ hobbynet/src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Box,
+  Stack,
+  Heading,
+  Flex,
+  Text,
+  Button,
+  useDisclosure
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const Navbar = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleToggle = () => (isOpen ? onClose() : onOpen());
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={6}
+      bg="teal.500"
+      color="white"
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+          Chakra UI
+        </Heading>
+      </Flex>
+
+      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+        <HamburgerIcon />
+      </Box>
+
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        display={{ base: isOpen ? "block" : "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Text>Docs</Text>
+        <Text>Examples</Text>
+        <Text>Blog</Text>
+      </Stack>
+
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button
+          variant="outline"
+          _hover={{ bg: "teal.700", borderColor: "teal.700" }}
+        >
+          Create account
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
