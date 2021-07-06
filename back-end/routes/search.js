0be943ac4e258c575db3edda3daeb6127763ea22
Jaemin Han <j4emin.han@gmail.com>
2021-07-06 18:50:58 -0400

    updated routing for search

--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 module.exports = (db) => {
 
-  router.get(`/`, (req, res) => {
+  router.get(`/:search`, (req, res) => {
       const queryString = `SELECT * FROM users 
       JOIN users_interests ON users.id = users_interests.user_id 
       JOIN interests ON users_interests.interest_id = interests.id 
-      JOIN categories ON interests.category_id = categories.id`
-    db.query(queryString)
+      JOIN categories ON interests.category_id = categories.id
+      WHERE interests.name LIKE '%$1%'
+      OR users.first_name LIKE '%$1%'
+      OR users.last_name LIKE '%$1%'`
+    db.query(queryString, [req.param.search])
     .then(
       (data) => {res.send(data.rows)})
   })
