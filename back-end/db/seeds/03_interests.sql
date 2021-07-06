f9f2d31da3be97f450687ad8e91809ba43a6d75b
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-07-06 14:02:17 -0400

    removing primary key so that it auto generates

--- back-end/db/seeds/03_interests.sql
+++ back-end/db/seeds/03_interests.sql
@@ -1,13 +1,13 @@
-insert into interests (id, category_id, name) values (1, 3, 'painting');
-insert into interests (id, category_id, name) values (2, 5, 'jazz');
-insert into interests (id, category_id, name) values (3, 1, 'tennis');
-insert into interests (id, category_id, name) values (4, 2, 'French');
-insert into interests (id, category_id, name) values (5, 3, 'whittling');
-insert into interests (id, category_id, name) values (6, 1, 'running');
-insert into interests (id, category_id, name) values (7, 5, 'singing');
-insert into interests (id, category_id, name) values (8, 4, 'computer science');
-insert into interests (id, category_id, name) values (9, 2, 'Spanish');
-insert into interests (id, category_id, name) values (10, 2, 'Russian');
+insert into interests (category_id, name) values (3, 'painting');
+insert into interests (category_id, name) values (5, 'jazz');
+insert into interests (category_id, name) values (1, 'tennis');
+insert into interests (category_id, name) values (2, 'French');
+insert into interests (category_id, name) values (3, 'whittling');
+insert into interests (category_id, name) values (1, 'running');
+insert into interests (category_id, name) values (5, 'singing');
+insert into interests (category_id, name) values (4, 'computer science');
+insert into interests (category_id, name) values (2, 'Spanish');
+insert into interests (category_id, name) values (2, 'Russian');
 
 
 -- insert into categories (id, name) values (1, 'sports');
