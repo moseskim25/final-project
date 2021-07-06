84972b6b4b5364174e3a8ab84260bb48ea157189
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-07-06 14:00:52 -0400

    styling on categories selection

--- hobbynet/src/components/Registration/Categories.js
+++ hobbynet/src/components/Registration/Categories.js
@@ -9,6 +9,7 @@ export default function Categories(props) {
 
   const [selected, setCategory] = useState('');
 
+  // selectedImg sports category_image hvr-grow
   return (
     <ChakraProvider>
       <VStack>
@@ -16,15 +17,35 @@ export default function Categories(props) {
           Pick a category! Don't worry you can always select more later
         </p>
         <Stack direction='row' >
-          <Image className='category_image' borderRadius="full" boxSize="95px" src="https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Segun Adebayo" onClick={() => {setCategory('sports')}}/>
-          <Image className='category_image' borderRadius="full" boxSize="95px" src="https://images.pexels.com/photos/2479312/pexels-photo-2479312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Segun Adebayo" onClick={() => {setCategory('music')}}/>
-          <Image className='category_image' borderRadius="full" boxSize="95px" src="https://images.pexels.com/photos/3778179/pexels-photo-3778179.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Segun Adebayo" onClick={() => {setCategory('art')}}/>
-          <Image className='category_image' borderRadius="full" boxSize="95px" src="https://images.pexels.com/photos/5984620/pexels-photo-5984620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Segun Adebayo" onClick={() => {setCategory('academic')}}/>
-          <Image className='category_image' borderRadius="full" boxSize="95px" src="https://images.pexels.com/photos/5984620/pexels-photo-5984620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Segun Adebayo" onClick={() => {setCategory('languages')}}/>
+          <VStack>
+            <Image className={`${selected === 'sports' ? 'selectedImg ' : ''}` + 'category_image hvr-grow'} src="https://images.pexels.com/photos/8224716/pexels-photo-8224716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Segun Adebayo" 
+              onClick={() => {
+                setCategory('sports')
+              }}/>
+            <p>Sports and Recreation</p>
+          </VStack>
+          <VStack>
+            <Image className={`${selected === 'music' ? 'selectedImg ' : ''}` + 'category_image hvr-grow'} src="https://images.pexels.com/photos/6671709/pexels-photo-6671709.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Segun Adebayo" onClick={() => {setCategory('music')}}/>
+            <p>Music</p>
+          </VStack>
+          <VStack>
+            <Image className={`${selected === 'art' ? 'selectedImg ' : ''}` + 'category_image hvr-grow'} src="https://images.pexels.com/photos/542556/pexels-photo-542556.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Segun Adebayo" onClick={() => {setCategory('art')}}/>
+            <p>Art</p>
+          </VStack>
+          <VStack>
+            <Image className={`${selected === 'academic' ? 'selectedImg ' : ''}` + 'category_image hvr-grow'} src="https://images.pexels.com/photos/53621/calculator-calculation-insurance-finance-53621.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Segun Adebayo" onClick={() => {setCategory('academic')}}/>
+            <p>Academics</p>
+          </VStack>
+          <VStack>
+            <Image className={`${selected === 'languages' ? 'selectedImg ' : ''}` + 'category_image hvr-grow'} src="https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Segun Adebayo" onClick={() => {setCategory('languages')}}/>
+            <p>Languages</p>
+          </VStack>
         </Stack>
         <p>{selected}</p>
-        <HStack>
-          <Box onClick={goBack}>Back</Box><Box onClick={() => goNext(selected)}>Next</Box>
+        <HStack className='registration_navigate'>
+          <Box className='back_next' onClick={goBack}>Back</Box>
+          <p> </p>
+          <Box className='back_next' onClick={() => goNext(selected)}>Next</Box>
         </HStack>
       </VStack>
     </ChakraProvider>
