7e8415e11f9f18a953ae4807ae41c5a066a5a7c8
Claire Devlin <claire.devlin@hotmail.com>
2021-07-07 14:10:50 -0400

    Get user photo and name in navbar, plus logout

--- hobbynet/src/hooks/helper.js
+++ hobbynet/src/hooks/helper.js
@@ -18,15 +18,12 @@ const helper = () => {
         // console.log(res.data.id);
         cookies.set("user_id", res.data.id, '/')
       });
-
   }
 
   const createUserGeneral = (first_name, last_name, postal_code) => {
     const id = cookies.get('user_id');
-
     const data = { first_name, last_name, postal_code, id };
     return axios.put('http://localhost:8000/users/new/general', data);
-
   }
 
   const getInterests = (category_name) => {
@@ -45,12 +42,8 @@ const helper = () => {
   const getConversations = (user_id) => {
     return axios.get(`http://localhost:8000/users/${user_id}/conversations`)
   }
-  
-  return { createUser, createUserGeneral, getInterests, setUserInterests, getConversations };
-  
+
+  return { createUser, createUserGeneral, getInterests, setUserInterests, getConversations, getUserInfo };
 }
-
-
-
 
 export default helper;
