7964c3dd6a3d79bba7bd08e951274ca06c13e2c6
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-07-07 14:21:40 -0400

    chat display

--- hobbynet/src/components/Chat/Chats.jsx
+++ hobbynet/src/components/Chat/Chats.jsx
@@ -190,8 +190,10 @@ export default function Main() {
         </ul>
         <footer>
           <textarea placeholder="Type your message"></textarea>
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="" />
+          <div>
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="" />
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="" />
+          </div>
           <a href="#">Send</a>
         </footer>
       </main>
