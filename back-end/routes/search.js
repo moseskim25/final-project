d33f5ae9e7b555e0799ea89c5da5a652da7484d2
Jaemin Han <j4emin.han@gmail.com>
2021-07-06 22:03:12 -0400

    create api endpoint for get request to /search that makes a query to the db

--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -8,12 +8,28 @@ module.exports = (db) => {
       JOIN users_interests ON users.id = users_interests.user_id 
       JOIN interests ON users_interests.interest_id = interests.id 
       JOIN categories ON interests.category_id = categories.id
-      WHERE interests.name LIKE '%$1%'
-      OR users.first_name LIKE '%$1%'
-      OR users.last_name LIKE '%$1%'`
-    db.query(queryString, [req.param.search])
+      WHERE interests.name LIKE $1
+      OR users.first_name LIKE $1
+      OR users.last_name LIKE $1;`
+    db.query(queryString, [`%${req.params.search}%`])
     .then(
       (data) => {res.send(data.rows)})
   })
   return router;
 };
+
+// module.exports = (db) => {
+
+//   router.get(`/:category`, (req, res) => {
+//     db.query(`SELECT * FROM categories
+//     WHERE name = $1;`,
+//     [req.params.category])
+//     .then(data => {
+//       db.query(`SELECT * FROM interests
+//       WHERE category_id = ${data.rows[0].id}`)
+//       .then(data => res.json(data.rows))
+//     })
+//     .catch(err => console.error(err));
+//   });
+//   return router;
+// };
