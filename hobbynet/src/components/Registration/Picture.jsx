1b6d737bcc9617bb5a1d96039f639d758381244f
Moses Kim <63984177+mosikim@users.noreply.github.com>
2021-07-11 15:28:22 -0400

    upload image styling and preview

--- hobbynet/src/components/Registration/Picture.jsx
+++ hobbynet/src/components/Registration/Picture.jsx
@@ -1,37 +1,52 @@
 import React, { useState } from "react";
-import { Center } from "@chakra-ui/react";
 import axios from "axios";
-import Cloud from "./Cloud";
-import Cookies from 'universal-cookie';
-import '../styles/Picture.scss';
+import Cookies from "universal-cookie";
+import "./Picture.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const cookies = new Cookies();
 
 export default function Picture() {
+  const user_id = Number(cookies.get("user_id"));
+  const [imageSelected, setImageSelected] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  const user_id = Number(cookies.get('user_id'));
-
-  const [imageSelected, setImageSelected] = useState('');
-  const [imagePreview, setImagePreview] = useState('');
-
-  const uploadImage = () => {
+  const uploadImage = (image) => {
     const formData = new FormData();
-    formData.append("file", imageSelected);
+    formData.append("file", image);
     formData.append("upload_preset", "c26irsnw");
 
-    axios.post("https://api.cloudinary.com/v1_1/dm4r202h4/image/upload", formData)
-      .then((res) => {
-        const image_url = res.data.url;
-        setImagePreview(image_url);
-        axios.put('http://localhost:8000/users/new/photo', { image_url, user_id })
-      });
+    axios.post("https://api.cloudinary.com/v1_1/dm4r202h4/image/upload", formData).then((res) => {
+      const image_url = res.data.url;
+      setImageUrl(image_url);
+    });
   };
 
-  return (
-    <Center>
-      <input type="file" onChange={(event) => setImageSelected(event.target.files[0])} />
-      <button onClick={uploadImage}>Upload Image</button>
+  const onSubmit = () => {
+    axios.put("http://localhost:8000/users/new/photo", { imageUrl, user_id });
+  };
 
-      <img src={imagePreview} alt='' className='profile_image_preview'/>
-    </Center>
+  const showPreview = !imageUrl ? (
+    <div className="upload-pic-box">
+      <div>
+        <label className="custom-file-upload">
+          <i className="fas fa-file-upload upload-icon"></i>
+          <input type="file" onChange={(event) => uploadImage(event.target.files[0])} />
+        </label>
+      </div>
+      <p>Upload an image</p>
+    </div>
+  ) : <img src={imageUrl} alt="" className="profile_image_preview" />;
+
+  return (
+    <div className="upload-pic">
+      <p className="description">Upload a profile picture</p>
+
+      {showPreview}
+
+      <button onSubmit={onSubmit} className="submit-button">
+        Submit
+      </button>
+
+    </div>
   );
 }
