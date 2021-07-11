23951ce995c959c10fab83ac4d6cc18c2589f441
Claire Devlin <claire.devlin@hotmail.com>
2021-07-11 13:32:05 -0400

    Added seeds

--- back-end/db/seeds/05_conversations.sql
+++ back-end/db/seeds/05_conversations.sql
@@ -1,4 +1,13 @@
 insert into conversations (user1_id, user2_id) values (1, 2);
 insert into conversations (user1_id, user2_id) values (2, 4);
 insert into conversations (user1_id, user2_id) values (3, 4);
+insert into conversations (user1_id, user2_id) values (1, 3);
+
+insert into conversations (user1_id, user2_id) values (1, 4);
+insert into conversations (user1_id, user2_id) values (1, 5);
+insert into conversations (user1_id, user2_id) values (2, 5);
+insert into conversations (user1_id, user2_id) values (2, 6);
+insert into conversations (user1_id, user2_id) values (2, 7);
+insert into conversations (user1_id, user2_id) values (3, 8);
+insert into conversations (user1_id, user2_id) values (3, 9);
 
--- back-end/db/seeds/06_messages.sql
+++ back-end/db/seeds/06_messages.sql
@@ -1,11 +1,14 @@
-insert into messages (conversations_id, sender_id, time, text, pinned) values (1, 1, '2021-07-03 11:13:44', 'Hi there! this is the first message', false);
+insert into messages (conversations_id, sender_id, time, text, pinned) values (1, 1, '2021-07-03 11:13:44', 'Hi Moses! Im learning to whittle and Im looking for a friend to help keep me motivated!', false);
+insert into messages (conversations_id, sender_id, time, text, pinned) values (1, 2, '2021-07-03 11:14:06', 'So would you say youre... a whittle bit lonely?', false);
 
-insert into messages (conversations_id, sender_id, time, text, pinned) values (1, 2, '2021-07-03 11:14:06', 'Hello from user 2, this is the second message', false);
-
-insert into messages (conversations_id, sender_id, time, text, pinned) values (2, 2, '2021-07-03 11:13:44', 'Second message', false);
-
+insert into messages (conversations_id, sender_id, time, text, pinned) values (2, 2, '2021-07-03 11:13:44', 'Hey!', false);
 insert into messages (conversations_id, sender_id, time, text, pinned) values (2, 4, '2021-07-03 11:14:06', 'what do you want', false);
 
 insert into messages (conversations_id, sender_id, time, text, pinned) values (3, 3, '2021-07-04 11:20:44', 'Hi from user 3! this is the first message', false);
-
 insert into messages (conversations_id, sender_id, time, text, pinned) values (3, 4, '2021-07-04 11:21:06', 'Hello from user 4, this is the second message', false);
+
+insert into messages (conversations_id, sender_id, time, text, pinned) values (4, 1, '2021-07-04 11:21:06', 'Hi Jaemin!', false);
+insert into messages (conversations_id, sender_id, time, text, pinned) values (4, 3, '2021-07-04 11:21:06', 'Hey Claire do you like jazz', false);
+
+insert into messages (conversations_id, sender_id, time, text, pinned) values (5, 4, '2021-07-04 11:21:06', 'Hello', false);
+insert into messages (conversations_id, sender_id, time, text, pinned) values (5, 5, '2021-07-04 11:21:06', 'Hi', false);
