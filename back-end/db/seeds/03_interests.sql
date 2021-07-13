c9bf47801b70276b4dfc0ae86916dfbe55f57d0e
Claire Devlin <claire.devlin@hotmail.com>
2021-07-12 21:35:56 -0400

    New seeds

--- back-end/db/seeds/03_interests.sql
+++ back-end/db/seeds/03_interests.sql
@@ -1,26 +1,25 @@
-insert into interests (category_id, name, image) values (3, 'painting', 'https://images.pexels.com/photos/374009/pexels-photo-374009.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
-
-insert into interests (category_id, name, image) values (5, 'jazz', 'https://images.pexels.com/photos/359995/pexels-photo-359995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-
 insert into interests (category_id, name, image) values (1, 'tennis', 'https://images.pexels.com/photos/5730288/pexels-photo-5730288.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
+insert into interests (category_id, name, image) values (1, 'running', 'https://images.pexels.com/photos/936094/pexels-photo-936094.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
+insert into interests (category_id, name, image) values (1, 'biking', 'https://res.cloudinary.com/dm4r202h4/image/upload/v1626107380/lfsvokcw8b4mih4xfqac.png');
 
 insert into interests (category_id, name, image) values (2, 'French', 'https://images.pexels.com/photos/6161502/pexels-photo-6161502.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
-
-insert into interests (category_id, name, image) values (3, 'whittling', 'https://images.pexels.com/photos/5124843/pexels-photo-5124843.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
-
-insert into interests (category_id, name, image) values (1, 'running', 'https://images.pexels.com/photos/936094/pexels-photo-936094.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
-
-insert into interests (category_id, name, image) values (5, 'singing', 'https://images.pexels.com/photos/2531728/pexels-photo-2531728.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-
-insert into interests (category_id, name, image) values (4, 'computer science', 'https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-
 insert into interests (category_id, name, image) values (2, 'Spanish', 'https://images.pexels.com/photos/54097/spain-flag-flutter-spanish-54097.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-
 insert into interests (category_id, name, image) values (2, 'Russian', 'https://cdn.britannica.com/42/3842-004-F47B77BC/Flag-Russia.jpg');
 
-insert into interests (category_id, name, image) values (3, 'Baking', 'https://res.cloudinary.com/dm4r202h4/image/upload/v1626123965/ux15kvbij8wxmusgwxac.png');
+insert into interests (category_id, name, image) values (3, 'painting', 'https://images.pexels.com/photos/374009/pexels-photo-374009.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
+insert into interests (category_id, name, image) values (3, 'whittling', 'https://images.pexels.com/photos/5124843/pexels-photo-5124843.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500');
+insert into interests (category_id, name, image) values (3, 'baking', 'https://res.cloudinary.com/dm4r202h4/image/upload/v1626123965/ux15kvbij8wxmusgwxac.png');
 
-insert into interests (category_id, name, image) values (1, 'Biking', 'https://res.cloudinary.com/dm4r202h4/image/upload/v1626107380/lfsvokcw8b4mih4xfqac.png');
+insert into interests (category_id, name, image) values (4, 'computer science', 'https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+insert into interests (category_id, name, image) values (4, 'history', 'https://images.pexels.com/photos/951531/pexels-photo-951531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+insert into interests (category_id, name, image) values (4, 'philosophy', 'https://images.pexels.com/photos/34592/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+
+insert into interests (category_id, name, image) values (5, 'jazz', 'https://images.pexels.com/photos/359995/pexels-photo-359995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+insert into interests (category_id, name, image) values (5, 'voice', 'https://images.pexels.com/photos/2531728/pexels-photo-2531728.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+insert into interests (category_id, name, image) values (5, 'rock', 'https://images.pexels.com/photos/164693/pexels-photo-164693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+
+
+
 
 
 -- insert into categories (id, name) values (1, 'sports');
